a0d078e5fdd8ca8605777ba7295045bd99a130c2
Alina <fetisovaalina505@gmail.com>
2025-05-08 20:59:29 +0300

    delete script.js

--- javascript.js/script.js
+++
@@ -1,7 +0,0 @@
-const slider = document.querySelector('.images');
-document.querySelector('.next').addEventListener('click', () => {
-  slider.scrollBy({ left: 250, behavior: 'smooth' });
-});
-document.querySelector('.prev').addEventListener('click', () => {
-  slider.scrollBy({ left: -250, behavior: 'smooth' });
-});
